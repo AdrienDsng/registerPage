8ffdb24b026f63c80e4dc52e3c9b09aac15d8c05
Adrien dossang <adrien.dossang@viacesi.fr>
2019-08-06 15:09:38 +0200

    Added email + reCaptcha checking
    - db insert on valid entries
    - page redirection and/or message display on error or success

---
+++ check.php
@@ -0,0 +1,48 @@
+<?php
+//calling the function
+checkForm();
+
+function checkForm(){
+
+    //checking if POST variable is empty
+    if(!empty($_POST)){
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $captcha = $_POST['g-recaptcha-response'];
+
+        //API request for email and captcha validation
+        $emailResponse = file_get_contents('https://open.kickbox.com/v1/disposable/'.$email);
+        $captchaResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.urlencode('6LfFl7EUAAAAADgfvjUvQZkYN7tgoukT8cAR_6YU').'&response='.urlencode($captcha));
+
+        $emailResponse = json_decode($emailResponse, true);
+        $captchaResponse = json_decode($captchaResponse, true);
+
+        if(!$emailResponse['disposable'] && $captchaResponse['success']){
+
+            $link = mysqli_connect("localhost", "root", "", "registerPage");
+
+            // Checking the connection
+            if($link === false){
+                die("ERROR: Could not connect. " . mysqli_connect_error());
+            }
+
+            // Attempt insert query execution
+            $sql = "INSERT INTO users (username, email) VALUES ('".$username."', '".$email."')";
+            if(mysqli_query($link, $sql)){
+                echo "User registered successfully.";
+            } else{
+                echo "ERROR: The command could not be executed $sql. " . mysqli_error($link);
+            }
+        }
+        else{
+            //redirection to register page due to error in reCaptcha or email address
+            header('Location: home.html');
+
+        }
+    }
+
+}
+
+
+
+
